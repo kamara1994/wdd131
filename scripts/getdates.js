32382fea4dfa212d978304245acc4109d6b25d47
kamara1994 <kam22005@byui.edu>
2024-11-09 01:19:51 -0700

    Initial commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set current year dynamically
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set last modified date dynamically
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
